f314efb8298c922d05d8ec3968a4c24c79e2e9f6
Vũ Đỗ <88678046+ImVux21@users.noreply.github.com>
2022-04-05 07:55:44 +0700

    Chatty Bot welcomes you

---
+++ Simple Chatty Bots/stage1.java
@@ -0,0 +1,10 @@
+package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        // write your code here
+        String name = "Aid";
+        int birthYear = 2020;
+        System.out.printf("Hello! My name is %s.\nI was created in %d.",name, birthYear);
+    }
+}
