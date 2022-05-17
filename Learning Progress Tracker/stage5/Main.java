ef791b072e641501d3c701e5589490b2b07b88b6
Vũ Đỗ <88678046+ImVux21@users.noreply.github.com>
2022-05-17 23:51:12 +0700

    Stage 5/5: Notification service

---
+++ Learning Progress Tracker/stage5/Main.java
@@ -0,0 +1,39 @@
+package tracker;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Learning Progress Tracker");
+        StudentManager sm = new StudentManager();
+        boolean stop = false;
+
+        while (!stop) {
+            String input = scanner.nextLine();
+
+            if (hasInput(input)) {
+                switch (input) {
+                    case "add students": sm.addStudentCommand(); break;
+                    case "list": sm.listStudentCommand(); break;
+                    case "add points": sm.addPointCommand(); break;
+                    case "find": sm.findStudentCommand(); break;
+                    case "statistics": Course.calculateStatisticsCommand(); break;
+                    case "notify": Course.notifyCompletedCourseCommand(); break;
+                    case "back":
+                        System.out.println("Enter 'exit' to exit the program.");
+                        break;
+                    case "exit": stop = true; break;
+                    default: System.out.println("Unknown command.");
+                }
+            } else {
+                System.out.println("No input.");
+            }
+        }
+        System.out.println("Bye!");
+    }
+
+    private static boolean hasInput(String input) {
+        return !input.isBlank(); // if blank, return false
+    }
+}
