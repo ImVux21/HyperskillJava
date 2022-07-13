518d1b8f6482d96d6e2bb85c50eae8ebd6facafb
Vũ Đỗ <88678046+ImVux21@users.noreply.github.com>
2022-07-13 19:08:43 +0700

    Stage 3/4: Save a text file

--- File Server/Stage 3: Save a text file/server/Main.java
+++ File Server/Stage 3: Save a text file/server/Main.java
@@ -6,6 +6,7 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -18,47 +19,44 @@ public class Main {
     private static final String DATA_STORED_DIR = "D:\\FIT_2022\\DSA_2022\\File Server\\File Server\\task\\src\\server\\data\\";
 
     public static void main(String[] args) {
-        boolean isStopped = true;
-
         try (
-                ServerSocket server = new ServerSocket(port, 50, InetAddress.getByName(ADDRESS));
-                Socket socket = server.accept();
-                DataInputStream input = new DataInputStream(socket.getInputStream());
-                DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+                ServerSocket server = new ServerSocket(port, 50, InetAddress.getByName(ADDRESS))
         ) {
+            while (true) {
+                try (Socket socket = server.accept();
+                     DataInputStream input = new DataInputStream(socket.getInputStream());
+                     DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+                ) {
+                    String clientRequest = input.readUTF();
+                    Action action = parseActionFromRequest(clientRequest);
 
-            while (isStopped) {
-                String clientRequest = input.readUTF();
-                Action action = parseActionFromRequest(clientRequest);
+                    String[] parts = clientRequest.split("\\s");
+                    String pathOfFile = null;
+                    if (parts.length > 1) {
+                        pathOfFile = DATA_STORED_DIR + parts[1];
+                    }
 
-                String[] parts = clientRequest.split("\\s");
-                String pathOfFile = null;
-                if (parts.length > 1) {
-                    pathOfFile = DATA_STORED_DIR + parts[1];
+                    // create server response based on the client request
+                    String serverResponse = null;
+                    switch (action) {
+                        case GET:
+                            serverResponse = handleGetRequest(pathOfFile);
+                            break;
+                        case PUT:
+                            serverResponse = handlePutRequest(pathOfFile, parts[2]);
+                            break;
+                        case DELETE:
+                            serverResponse = handleDeleteRequest(pathOfFile);
+                            break;
+                        case EXIT:
+                            // shut down the server
+                            System.exit(0);
+                    }
+                    // send the server response back to client
+                    sendServerResponse(serverResponse, output);
+
                 }
-
-                // create server response based on the client request
-                String serverResponse = null;
-                switch (action) {
-                    case GET:
-                        serverResponse = handleGetRequest(pathOfFile);
-                        break;
-                    case PUT:
-                        serverResponse = handlePutRequest(pathOfFile, parts[2]);
-                        break;
-                    case DELETE:
-                        serverResponse = handleDeleteRequest(pathOfFile);
-                        break;
-                    case EXIT:
-                        // shut down the server
-                        isStopped = false;
-                        System.exit(0);
-
-                }
-                // send the server response back to client
-                sendServerResponse(serverResponse, output);
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -91,7 +89,7 @@ public class Main {
 
     private static String getContentFromFile(String path) {
         try {
-            return new String(Files.readAllBytes(Paths.get(path))).trim();
+            return new String(Files.readAllBytes(Paths.get(path)));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -99,23 +97,17 @@ public class Main {
     }
 
     private static String handlePutRequest(String pathOfFile, String content) {
-            File file = new File(pathOfFile);
+        if (!isFileExists(pathOfFile)) {
+            writeContentToFile(pathOfFile, content);
+            return "200";
+        }
 
-            try {
-                if (file.createNewFile()) {
-                    writeContentToFile(file, content);
-                    return "200";
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return "403";
+        return "403";
     }
 
-    private static void writeContentToFile(File file, String content) {
-        try (FileWriter writer = new FileWriter(file)) {
-            writer.write(content);
+    private static void writeContentToFile(String pathOfFile, String content) {
+        try {
+            Files.write(Path.of(pathOfFile), content.getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -142,7 +134,6 @@ public class Main {
     }
 
     private static boolean isFileExists(String path) {
-        Path of = Paths.get(path);
-        return Files.exists(of) && !Files.isDirectory(of);
+        return Files.exists(Path.of(path));
     }
 }
