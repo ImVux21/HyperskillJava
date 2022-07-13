5196072d25ad83bce45c4b81b981be52245ed661
Vũ Đỗ <88678046+ImVux21@users.noreply.github.com>
2022-07-13 07:08:58 +0700

    Update Main.java

--- File Server/Stage 3: Save a text file/server/Main.java
+++ File Server/Stage 3: Save a text file/server/Main.java
@@ -1,61 +1,148 @@
 package server;
 
+import action.Action;
+
 import java.io.*;
-import java.net.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
+
     private static final String ADDRESS = "127.0.0.1";
-    private static String[] code = {"200", "403", "404"};
-    private static final int PORT = 23456;
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(PORT, 50, InetAddress.getByName(ADDRESS));
-        Socket socket = serverSocket.accept();
+    private static final int port = 23456;
 
-        DataInputStream inputStream = new DataInputStream(socket.getInputStream());
-        DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
+    private static final String DATA_STORED_DIR = "D:\\FIT_2022\\DSA_2022\\File Server\\File Server\\task\\src\\server\\data\\";
 
-        String message = inputStream.readUTF(); // receive data from client
-        String[] l = message.split("\\s+");
+    public static void main(String[] args) {
+        boolean isStopped = true;
 
-        String fileName = "D:\\FIT_2022\\DSA_2022\\File Server\\File Server\\task\\src\\server\\data\\folder\\" + l[1];
-        Path path = Path.of(fileName);
+        try (
+                ServerSocket server = new ServerSocket(port, 50, InetAddress.getByName(ADDRESS));
+                Socket socket = server.accept();
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+        ) {
 
-        switch (l[0]) {
-            case "GET":
-                if (Files.exists(path)) {
-                    outputStream.writeUTF(code[0]);
-                } else {
-                    outputStream.writeUTF(code[2]);
+            while (isStopped) {
+                String clientRequest = input.readUTF();
+                Action action = parseActionFromRequest(clientRequest);
+
+                String[] parts = clientRequest.split("\\s");
+                String pathOfFile = null;
+                if (parts.length > 1) {
+                    pathOfFile = DATA_STORED_DIR + parts[1];
                 }
-                break;
 
-            case "DELETE":
-                if (Files.deleteIfExists(path)) {
-                    outputStream.writeUTF(code[0]);
-                } else {
-                    outputStream.writeUTF(code[2]);
-                }
-                break;
+                // create server response based on the client request
+                String serverResponse = null;
+                switch (action) {
+                    case GET:
+                        serverResponse = handleGetRequest(pathOfFile);
+                        break;
+                    case PUT:
+                        serverResponse = handlePutRequest(pathOfFile, parts[2]);
+                        break;
+                    case DELETE:
+                        serverResponse = handleDeleteRequest(pathOfFile);
+                        break;
+                    case EXIT:
+                        // shut down the server
+                        isStopped = false;
+                        System.exit(0);
 
-            case "PUT":
-                if (!Files.exists(path)) {
-                    File file = new File(String.valueOf(path));
-                    FileWriter writer = new FileWriter(file);
-                    writer.write(l[2]);
-                    if (file.createNewFile()) {
-                        outputStream.writeUTF(code[0]);
-                    }
-                } else {
-                    outputStream.writeUTF(code[1]);
                 }
-                break;
+                // send the server response back to client
+                sendServerResponse(serverResponse, output);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
+    }
 
-//        inputStream.close();
-//        socket.close();
-//        serverSocket.close();
+    private static Action parseActionFromRequest(String clientRequest) {
+        String[] parts = clientRequest.split("\\s+");
+
+        if (parts[0].equals("GET")) {
+            return Action.GET;
+        } else if (parts[0].equals("PUT")) {
+            return Action.PUT;
+        } else if (parts[0].equals("DELETE")) {
+            return Action.DELETE;
+        } else if (parts[0].equals("exit")) {
+            return Action.EXIT;
+        }
+        return null;
+    }
+
+    private static String handleGetRequest(String path) {
+        if (isFileExists(path)) {
+            String content = getContentFromFile(path);
+            return "200 " + content;
+        }
+
+        return "404";
+    }
+
+    private static String getContentFromFile(String path) {
+        try {
+            return new String(Files.readAllBytes(Paths.get(path))).trim();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static String handlePutRequest(String pathOfFile, String content) {
+            File file = new File(pathOfFile);
+
+            try {
+                if (file.createNewFile()) {
+                    writeContentToFile(file, content);
+                    return "200";
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return "403";
+    }
+
+    private static void writeContentToFile(File file, String content) {
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String handleDeleteRequest(String pathOfFile) {
+        try {
+            if (Files.deleteIfExists(Paths.get(pathOfFile))) {
+                return "200";
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "404";
+    }
+
+    private static void sendServerResponse(String serverResponse, DataOutputStream output) {
+        try {
+            output.writeUTF(serverResponse);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean isFileExists(String path) {
+        Path of = Paths.get(path);
+        return Files.exists(of) && !Files.isDirectory(of);
     }
 }
