75e54dc6e06e5dd680b521a2a1d08f8846df7a1d
Vũ Đỗ <88678046+ImVux21@users.noreply.github.com>
2022-10-06 20:05:13 +0700

    Update Main.java

--- File Server/Stage 4: Save anything/server/Main.java
+++ File Server/Stage 4: Save anything/server/Main.java
@@ -1,6 +1,6 @@
 package server;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -12,17 +12,19 @@ import java.util.concurrent.Executors;
 
 public class Main {
     private static final String ADDRESS = "127.0.0.1";
+    private static final String DATA_SAVED = "D:\\FIT_2022\\DSA_2022\\File Server\\File Server\\task\\src\\server\\map.txt";
     private static final int PORT = 23456;
 
     public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(PORT, 50, InetAddress.getByName(ADDRESS))) {
             ExecutorService executor = Executors.newFixedThreadPool(10);
 
+            LinkedHashMap<Integer, String> mapFiles = parseMapFiles();
+
             while (true) {
                 Socket socket = server.accept();
                 Session session = new Session(socket, executor);
 
-                LinkedHashMap<Integer, String> mapFiles = parseMapFiles();
                 Session.setMapFiles(mapFiles);
 
                 executor.submit(session);
@@ -33,8 +35,9 @@ public class Main {
         }
     }
 
+
     private static LinkedHashMap<Integer, String> parseMapFiles() throws IOException {
-        Scanner sc = new Scanner(Paths.get("D:\\FIT_2022\\DSA_2022\\File Server\\File Server\\task\\src\\server\\map.txt"));
+        Scanner sc = new Scanner(Paths.get(DATA_SAVED));
         LinkedHashMap<Integer, String> mapFiles = new LinkedHashMap<>();
 
         while (sc.hasNext()) {
@@ -47,4 +50,5 @@ public class Main {
 
         return mapFiles;
     }
+
 }
